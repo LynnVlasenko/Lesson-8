9831c09a9587881a5c7c91054d8664a775f20479
LynnVlasenko <lynn.vlasenko@gmail.com>
2024-03-14 18:13:50 +0100

    Add new project

---
+++ Lesson8/Lesson8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson8
+//
+//  Created by Алина Власенко on 14.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
